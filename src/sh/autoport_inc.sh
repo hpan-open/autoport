228af68e6b3fb5c8a9ff3d3a89b734849f82ef27
Sean Champ <spchamp@users.noreply.github.com>
2016-01-21 17:36:10 -0800

    update jail name syntax; add notes about jail maintenance

--- src/sh/autoport_inc.sh
+++ src/sh/autoport_inc.sh
@@ -17,20 +17,30 @@ VERSION=${VERSION:-$(uname -r)}
 
 ARCH=${ARCH:-$(uname -m)}
 
-JAIL=${JAIL:-$(echo $VERSION | sed -E 's!(\.[0-9]+)-(RELEASE|STABLE)!\1-!;s!\.!-!')${ARCH}}
+JAIL=${JAIL:-$(echo $VERSION | sed -E 's!(\.[0-9]+)-(RELEASE|STABLE)!\1-!;s!\.!-!')_${ARCH}}
 
 ## FIXME: what is/was the 'gjb' jail method?
 #JHOW=${JHOW:-ftp}
 ## svn must be installed first
 #JHOW=${JHOW:-svn+ssh}
 
+
 JHOW=${JHOW:-null -M /usr/local/jail}
+## cd /usr/src && for tgt in installworld distrib-dirs distribution; do make $tgt installworld DESTDIR=${JAIL_PATH} DB_FROM_SRC=1
+##
+## cp /etc/make.conf ${JAIL_PATH}
+##
+## ... modify for ccache ?, see :
+## https://fossil.etoilebsd.net/poudriere/doc/trunk/doc/ccache.wiki
+## 
+
+
 #JHOW=${JHOW:-src=/usr/src}
 
 
 #PHOW=${PHOW:-portsnap}
 ## To use the git method, of course git must be installed first
-PHOW=${PHOW:-git}
+PHOW=${PHOW:-portsnap}
 
 
 ## do we have to run portsnap first, independent of poudriere?
